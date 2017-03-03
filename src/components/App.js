7ad84c8cafd35286cb7250dfa568b1c4825d9640
krisaoe <krisopowell@gmail.com>
2017-03-04 05:33:31 +1100

    stable

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import styles from './App.css';
 const App = () => (
   <div className={styles.app}>
     <h2>Hello, Universe!</h2>
+    <div className={styles.circle}></div>
+    <div className={styles.isoscelesTriangle}></div>
+    <div className={styles.rightIsoscelesTriangle}></div>
+    <div className={styles.equilateralTriangle}></div>
   </div>
 );
 
